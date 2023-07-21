da7680f89a4da62a4e0aa3c966ffec3a34553f2c
Yellow953 <joemazloum953@gmail.com>
2023-07-21 23:24:04 +0300

    template separate + logos

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,51 @@
+<!-- HEADER DESKTOP-->
+<header class="header-desktop">
+    <div class="section__content section__content--p30">
+        <div class="container-fluid">
+            <div class="header-wrap">
+                <form class="form-header" action="" method="POST">
+
+                </form>
+                <div class="header-button">
+                    <div class="noti-wrap">
+                    </div>
+                    <div class="account-wrap">
+                        <div class="account-item clearfix js-item-menu">
+                            <div class="image">
+                                <img src="{{asset('assets/images/default_profile.png')}}" />
+                            </div>
+                            <div class="content">
+                                <a class="js-acc-btn" href="/">{{auth()->user()->name}}</a>
+                            </div>
+                            <div class="account-dropdown js-dropdown">
+                                <div class="info clearfix">
+                                    <div class="image">
+                                        <a href="#">
+                                            <img src="{{asset('assets/images/default_profile.png')}}" />
+                                        </a>
+                                    </div>
+                                    <div class="content">
+                                        <h5 class="name">
+                                            <a href="#">{{auth()->user()->name}}</a>
+                                        </h5>
+                                        <span class="email">{{auth()->user()->email}}</span>
+                                    </div>
+                                </div>
+                                <div class="account-dropdown__footer">
+                                    <a href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                                        <i class="zmdi zmdi-power"></i>Logout</a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- HEADER DESKTOP-->
