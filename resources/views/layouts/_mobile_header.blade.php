7a8fd6186d58768ed9522ca4a67fa00180d3498f
Yellow953 <joemazloum953@gmail.com>
2023-07-24 13:36:06 +0300

    nav icons

--- resources/views/layouts/_mobile_header.blade.php
+++ resources/views/layouts/_mobile_header.blade.php
@@ -18,21 +18,41 @@
     <nav class="navbar-mobile">
         <div class="container-fluid">
             <ul class="navbar-mobile__list list-unstyled bg-gray">
-                <li>
-                    <a href="/" class="active">
-                        <i class="fas fa-tachometer-alt"></i>Dashboard</a>
+                <li class="active">
+                    <a href="/" class="text-white d-flex">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" class="mr-4 my-auto"
+                            fill="currentColor" class="bi bi-speedometer" viewBox="0 0 16 16">
+                            <path
+                                d="M8 2a.5.5 0 0 1 .5.5V4a.5.5 0 0 1-1 0V2.5A.5.5 0 0 1 8 2zM3.732 3.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707zM2 8a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 8zm9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5zm.754-4.246a.389.389 0 0 0-.527-.02L7.547 7.31A.91.91 0 1 0 8.85 8.569l3.434-4.297a.389.389 0 0 0-.029-.518z" />
+                            <path fill-rule="evenodd"
+                                d="M6.664 15.889A8 8 0 1 1 9.336.11a8 8 0 0 1-2.672 15.78zm-4.665-4.283A11.945 11.945 0 0 1 8 10c2.186 0 4.236.585 6.001 1.606a7 7 0 1 0-12.002 0z" />
+                        </svg><span class="my-auto">Dashboard</span></a>
                 </li>
                 <li>
-                    <a href="/meetings" class="text-white">
-                        <i class="fas fa-chart-bar"></i>Meetings</a>
+                    <a href="/meetings" class="text-white d-flex">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" class="mr-4 my-auto"
+                            fill="currentColor" class="bi bi-calendar3" viewBox="0 0 16 16">
+                            <path
+                                d="M14 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1 .857H2c-.552 0-1-.384-1-.857V3.857z" />
+                            <path
+                                d="M6.5 7a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                        </svg><span class="my-auto text-uppercase">Meetings</span></a>
                 </li>
                 <li>
-                    <a href="/events" class="text-white">
-                        <i class="fas fa-chart-bar"></i>Events</a>
+                    <a href="/events" class="text-white d-flex">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" class="mr-4 my-auto"
+                            fill="currentColor" class="bi bi-cone-striped" viewBox="0 0 16 16">
+                            <path
+                                d="m9.97 4.88.953 3.811C10.159 8.878 9.14 9 8 9c-1.14 0-2.158-.122-2.923-.309L6.03 4.88C6.635 4.957 7.3 5 8 5s1.365-.043 1.97-.12zm-.245-.978L8.97.88C8.718-.13 7.282-.13 7.03.88L6.275 3.9C6.8 3.965 7.382 4 8 4c.618 0 1.2-.036 1.725-.098zm4.396 8.613a.5.5 0 0 1 .037.96l-6 2a.5.5 0 0 1-.316 0l-6-2a.5.5 0 0 1 .037-.96l2.391-.598.565-2.257c.862.212 1.964.339 3.165.339s2.303-.127 3.165-.339l.565 2.257 2.391.598z" />
+                        </svg><span class="my-auto text-uppercase">Events</span></a>
                 </li>
                 <li class="has-sub">
-                    <a class="js-arrow text-white" href="#">
-                        <i class="fas fa-copy"></i>Users</a>
+                    <a class="js-arrow text-white d-flex" href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" class="mr-4 my-auto"
+                            fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
+                            <path
+                                d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7Zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216ZM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
+                        </svg><span class="my-auto text-uppercase">Users</span></a>
                     <ul class="navbar-mobile-sub__list list-unstyled js-sub-list bg-black">
                         <li>
                             <a href="/users" class="text-white">Users</a>
@@ -46,12 +66,22 @@
                     </ul>
                 </li>
                 <li>
-                    <a href="/records" class="text-white">
-                        <i class="fas fa-chart-bar"></i>Records</a>
+                    <a href="/records" class="text-white d-flex">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" class="mr-4 my-auto"
+                            fill="currentColor" class="bi bi-stars" viewBox="0 0 16 16">
+                            <path
+                                d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828l.645-1.937zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.734 1.734 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.734 1.734 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.734 1.734 0 0 0 3.407 2.31l.387-1.162zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L10.863.1z" />
+                        </svg><span class="my-auto text-uppercase">Records</span></a>
                 </li>
                 <li>
-                    <a href="/results" class="text-white">
-                        <i class="fas fa-chart-bar"></i>Results</a>
+                    <a href="/results" class="text-white d-flex">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" class="mr-4 my-auto"
+                            fill="currentColor" class="bi bi-clipboard2-check-fill" viewBox="0 0 16 16">
+                            <path
+                                d="M10 .5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5V2a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 11 2v-.5a.5.5 0 0 0-.5-.5.5.5 0 0 1-.5-.5Z" />
+                            <path
+                                d="M4.085 1H3.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1h-.585c.055.156.085.325.085.5V2a1.5 1.5 0 0 1-1.5 1.5h-5A1.5 1.5 0 0 1 4 2v-.5c0-.175.03-.344.085-.5Zm6.769 6.854-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708.708Z" />
+                        </svg><span class="my-auto text-uppercase">Results</span></a>
                 </li>
             </ul>
         </div>
